2314682a5f3bb79f0d5726dac6f1d44af0df8bcf
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-09-12 19:27:39 +0100

    Add spring init files

---
+++ src/main/java/com/github/dgwatts/pidigits/PidigitsApplication.java
@@ -0,0 +1,13 @@
+package com.github.dgwatts.pidigits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidigitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidigitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dgwatts/pidigits/PidigitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dgwatts.pidigits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidigitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
