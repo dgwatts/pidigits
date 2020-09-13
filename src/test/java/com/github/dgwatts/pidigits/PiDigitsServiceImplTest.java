bca83355c4d006fad7d02bfb80ea17acab7d1915
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-09-13 17:14:44 +0100

    Clean up test code

--- src/test/java/com/github/dgwatts/pidigits/PiDigitsServiceImplTest.java
+++ src/test/java/com/github/dgwatts/pidigits/PiDigitsServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestPiDigitsServiceImpl {
@@ -14,29 +15,32 @@ public class TestPiDigitsServiceImpl {
 	// 5779458151
 	private static final char[] LAST_TEN = new char[]{'5', '7', '7', '9', '4', '5', '8', '1', '5', '1'};
 
+	private PiDigitsServiceImpl instanceUnderTest;
+
+	@BeforeEach
+	public void setup() {
+		instanceUnderTest = new PiDigitsServiceImpl();
+	}
+
 	@Test
 	public void testGetDigitFirstTenDigitsIndividually() throws IOException {
-		final PiDigitsServiceImpl piDigitsService = new PiDigitsServiceImpl();
+
 
 		for (int i = 0; i < FIRST_TEN.length; i++) {
-			assertEquals("digit " + (i), FIRST_TEN[i], piDigitsService.get(i).getValue());
+			assertEquals("digit " + (i), FIRST_TEN[i], instanceUnderTest.get(i).getValue());
 		}
 	}
 
 	@Test
 	public void testGetDigitLastTenDigitsIndividually() throws IOException {
-		final PiDigitsServiceImpl piDigitsService = new PiDigitsServiceImpl();
-
 		for (int i = 0; i < LAST_TEN.length; i++) {
-			assertEquals("digit " + (i+999990), LAST_TEN[i], piDigitsService.get(i+999990).getValue());
+			assertEquals("digit " + (i+999990), LAST_TEN[i], instanceUnderTest.get(i+999990).getValue());
 		}
 	}
 
 	@Test
 	public void testGetRangeFirstTenDigits() throws IOException {
-		final PiDigitsServiceImpl piDigitsService = new PiDigitsServiceImpl();
-
-		final PiDigit[] range = piDigitsService.getRange(0, 10);
+		final PiDigit[] range = instanceUnderTest.getRange(0, 10);
 		assertEquals("10 elements", FIRST_TEN.length, range.length);
 
 		for (int i = 0; i < range.length; i++) {
@@ -47,9 +51,7 @@ public class TestPiDigitsServiceImpl {
 
 	@Test
 	public void testGetRangeLastTenDigits() throws IOException {
-		final PiDigitsServiceImpl piDigitsService = new PiDigitsServiceImpl();
-
-		final PiDigit[] range = piDigitsService.getRange(999990, 1000000);
+		final PiDigit[] range = instanceUnderTest.getRange(999990, 1000000);
 		assertEquals("10 elements", LAST_TEN.length, range.length);
 
 		for (int i = 0; i < range.length; i++) {
@@ -60,9 +62,7 @@ public class TestPiDigitsServiceImpl {
 
 	@Test
 	public void testGetRangeSingleArgFirstTenDigits() throws IOException {
-		final PiDigitsServiceImpl piDigitsService = new PiDigitsServiceImpl();
-
-		final PiDigit[] range = piDigitsService.getRange(10);
+		final PiDigit[] range = instanceUnderTest.getRange(10);
 
 		assertEquals("10 elements", FIRST_TEN.length, range.length);
 
@@ -74,9 +74,7 @@ public class TestPiDigitsServiceImpl {
 
 	@Test
 	public void testGetDigitsEveryOtherDigitOfFirstTen() throws IOException {
-		final PiDigitsServiceImpl piDigitsService = new PiDigitsServiceImpl();
-
-		final PiDigit[] digits = piDigitsService.getDigits(new int[]{0, 2, 4, 6, 8});
+		final PiDigit[] digits = instanceUnderTest.getDigits(new int[]{0, 2, 4, 6, 8});
 
 		assertEquals("5 elements", 5, digits.length);
 
