8332e27838e283bb66760a10814a10c2c5c52c67
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-09-13 17:17:05 +0100

    Common some test stuff out to be able to reuse it

--- src/test/java/com/github/dgwatts/pidigits/PiDigitsServiceImplTest.java
+++ src/test/java/com/github/dgwatts/pidigits/PiDigitsServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.github.dgwatts.pidigits;
 
+import static com.github.dgwatts.pidigits.TestUtils.assertRangeEquals;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 import java.io.IOException;
@@ -7,13 +8,7 @@ import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestPiDigitsServiceImpl {
-
-	// 1415926535
-	private static final char[] FIRST_TEN = new char[]{'1', '4', '1', '5', '9', '2', '6', '5', '3', '5'};
-
-	// 5779458151
-	private static final char[] LAST_TEN = new char[]{'5', '7', '7', '9', '4', '5', '8', '1', '5', '1'};
+public class PiDigitsServiceImplTest {
 
 	private PiDigitsServiceImpl instanceUnderTest;
 
@@ -24,52 +19,42 @@ public class TestPiDigitsServiceImpl {
 
 	@Test
 	public void testGetDigitFirstTenDigitsIndividually() throws IOException {
-
-
-		for (int i = 0; i < FIRST_TEN.length; i++) {
-			assertEquals("digit " + (i), FIRST_TEN[i], instanceUnderTest.get(i).getValue());
+		for (int i = 0; i < TestUtils.FIRST_TEN.length; i++) {
+			assertEquals("digit " + (i), TestUtils.FIRST_TEN[i], instanceUnderTest.getDigit(i).getValue());
 		}
 	}
 
 	@Test
 	public void testGetDigitLastTenDigitsIndividually() throws IOException {
-		for (int i = 0; i < LAST_TEN.length; i++) {
-			assertEquals("digit " + (i+999990), LAST_TEN[i], instanceUnderTest.get(i+999990).getValue());
+		for (int i = 0; i < TestUtils.LAST_TEN.length; i++) {
+			assertEquals("digit " + (i+999990), TestUtils.LAST_TEN[i], instanceUnderTest.getDigit(i+999990).getValue());
 		}
 	}
 
 	@Test
 	public void testGetRangeFirstTenDigits() throws IOException {
 		final PiDigit[] range = instanceUnderTest.getRange(0, 10);
-		assertEquals("10 elements", FIRST_TEN.length, range.length);
-
-		for (int i = 0; i < range.length; i++) {
-			assertEquals("index " + i, i, range[i].getIndex());
-			assertEquals("digit " + i, FIRST_TEN[i], range[i].getValue());
-		}
+		assertEquals("10 elements", TestUtils.FIRST_TEN.length, range.length);
+		assertRangeEquals(range, TestUtils.FIRST_TEN);
 	}
+
+
 
 	@Test
 	public void testGetRangeLastTenDigits() throws IOException {
 		final PiDigit[] range = instanceUnderTest.getRange(999990, 1000000);
-		assertEquals("10 elements", LAST_TEN.length, range.length);
+		assertEquals("10 elements", TestUtils.LAST_TEN.length, range.length);
 
-		for (int i = 0; i < range.length; i++) {
-			assertEquals("index " + (i+999990), i+999990, range[i].getIndex());
-			assertEquals("digit " + i, LAST_TEN[i], range[i].getValue());
-		}
+		assertRangeEquals(range, TestUtils.LAST_TEN, 999990);
 	}
 
 	@Test
 	public void testGetRangeSingleArgFirstTenDigits() throws IOException {
 		final PiDigit[] range = instanceUnderTest.getRange(10);
 
-		assertEquals("10 elements", FIRST_TEN.length, range.length);
+		assertEquals("10 elements", TestUtils.FIRST_TEN.length, range.length);
 
-		for (int i = 0; i < range.length; i++) {
-			assertEquals("index " + i, i, range[i].getIndex());
-			assertEquals("digit " + i, FIRST_TEN[i], range[i].getValue());
-		}
+		assertRangeEquals(range, TestUtils.LAST_TEN);
 	}
 
 	@Test
@@ -80,7 +65,7 @@ public class TestPiDigitsServiceImpl {
 
 		for (int i = 0; i < digits.length; i++) {
 			assertEquals("index " + (i*2), i*2, digits[i].getIndex());
-			assertEquals("digit " + i*2, FIRST_TEN[i*2], digits[i].getValue());
+			assertEquals("digit " + i*2, TestUtils.FIRST_TEN[i*2], digits[i].getValue());
 		}
 	}
 }
---
+++ src/test/java/com/github/dgwatts/pidigits/TestUtils.java
@@ -0,0 +1,26 @@
+package com.github.dgwatts.pidigits;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+public class TestUtils {
+
+	// 1415926535
+	public static final char[] FIRST_TEN = new char[]{'1', '4', '1', '5', '9', '2', '6', '5', '3', '5'};
+
+	// 5779458151
+	public static final char[] LAST_TEN = new char[]{'5', '7', '7', '9', '4', '5', '8', '1', '5', '1'};
+
+	private TestUtils() { }
+
+	public static void assertRangeEquals(PiDigit[] range, char[] expected) {
+		assertRangeEquals(range, expected, 0);
+	}
+
+	public static void assertRangeEquals(PiDigit[] range, char[] expected, int shunt) {
+		for (int i = 0; i < range.length; i++) {
+			assertEquals("index " + i, i, range[i].getIndex());
+			assertEquals("digit " + i, expected[i], range[i].getValue());
+		}
+	}
+
+}
