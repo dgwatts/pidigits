204784c1b34c1e42666573e3625a36eb78f85e9d
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-09-13 12:13:50 +0100

    Add API

---
+++ src/main/java/com/github/dgwatts/pidigits/PiDigit.java
@@ -0,0 +1,19 @@
+package com.github.dgwatts.pidigits;
+
+public class PiDigit {
+	private final int index;
+	private final char value;
+
+	public PiDigit(int index, char value) {
+		this.index = index;
+		this.value = value;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public char getValue() {
+		return value;
+	}
+}
---
+++ src/main/java/com/github/dgwatts/pidigits/PiDigitsService.java
@@ -0,0 +1,37 @@
+package com.github.dgwatts.pidigits;
+
+import java.io.IOException;
+
+public interface PiDigitsService {
+	/**
+	 * Get a contiguous range of digits from the decimal portion of pi. Uses the semantics of String.substring(int, int),
+	 * eg  the range begins at the specified {@code start} digit and extends to the digit at index {@code end - 1}, meaning
+	 * the length of the range is {@code end - start}.
+	 * @param start The start index, inclusive.
+	 * @param end The end index, exclusive.
+	 * @return The specified range.
+	 */
+	PiDigit[] getRange(int start, int end) throws IOException;
+
+	/**
+	 * Get a contiguous range of digits from the decimal portion of pi, starting from the front. Equivalent to calling
+	 * getRange(0, end)
+	 * @param end The end index, exclusive.
+	 * @return The specified range.
+	 */
+	PiDigit[] getRange(int end) throws IOException;
+
+	/**
+	 * Get a single digit from the decimal portion of pi.
+	 * @param index The index of the digut to return
+	 * @return The specified digit.
+	 */
+	PiDigit get(int index) throws IOException;
+
+	/**
+	 * Get a selection of digits from the decimal portion of pi. The specified indices do not have to be contiguous or ordered
+	 * @param indices The indices of the digits to return
+	 * @return The specified digits.
+	 */
+	PiDigit[] getDigits(int[] indices) throws IOException;
+}
