9b11f20dcc46c239e7354009d4af1ab60ca5536d
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-09-16 15:17:08 +0100

    Adding google SSO

--- frontend/src/components/PiDigits/PiDigits.js
+++ frontend/src/components/PiDigits/PiDigits.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import DigitsOutput from "../DigitsOutput/DigitsOutput";
 import {submit} from "../../api";
+import GoogleLogin from "react-google-login";
 
 /**
  * The complete UI component for the Pi digit service
@@ -19,9 +20,32 @@ class PiDigits extends Component {
 		submit(document.getElementById("input").value).then((response => this.setState({response})));
 	}
 
+	responseGoogle = (googleUser) => {
+		console.log(googleUser);
+
+		const newState = {
+			name: googleUser.profileObj.name,
+			email: googleUser.profileObj.email,
+			tokenId: googleUser.tokenId,
+			accessToken: googleUser.accessToken,
+			tokenObj: googleUser.tokenObject
+		};
+
+		this.setState(newState);
+	}
+
 	render() {
 		return (
 			<>
+				<GoogleLogin
+					clientId="335352850054-8q3pd4moshd6353588o1gbectgop6rt2.apps.googleusercontent.com"
+					render={renderProps => (
+						<button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+					)}
+					buttonText="Login"
+					onSuccess={this.responseGoogle}
+					onFailure={this.responseGoogle}
+					cookiePolicy={'single_host_origin'}/>
 				<input id="input" type="text"/>
 				<button onClick={this.submit}>Submit</button>
 
